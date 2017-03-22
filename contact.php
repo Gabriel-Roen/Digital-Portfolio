21dfd52c8cb8fa66063b05ccd3cde6dcc2a1de61
Gabriel Storm <gstormburge@gmail.com>
2017-03-22 08:01:56 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,5 @@
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
